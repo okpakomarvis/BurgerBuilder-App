f03904d884b9efa30e150bc94f727efe5865c90b
okpakomarvis <okpakomarvis@gmail.com>
2019-10-16 13:11:58 +0100

    Added all Complete file

---
+++ build/precache-manifest.721f9c33cd88402d9aba2f6c573e2366.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7206d12682370a3eece845661ae0282c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bde87b40bba4a85c9ae",
+    "url": "/static/css/3.5e51c0ad.chunk.css"
+  },
+  {
+    "revision": "6e33be49251dd55fc69c",
+    "url": "/static/css/4.783a6c9b.chunk.css"
+  },
+  {
+    "revision": "2b2f1b095f59b1e77f96",
+    "url": "/static/css/5.3f06fc33.chunk.css"
+  },
+  {
+    "revision": "891f169d9386360ce797",
+    "url": "/static/css/6.3c736b40.chunk.css"
+  },
+  {
+    "revision": "71c3c61bf1a5fc7f4137",
+    "url": "/static/css/main.d4a5e638.chunk.css"
+  },
+  {
+    "revision": "2312b4b0f59d13e6709b",
+    "url": "/static/js/2.9f82f144.chunk.js"
+  },
+  {
+    "revision": "3bde87b40bba4a85c9ae",
+    "url": "/static/js/3.90d48c49.chunk.js"
+  },
+  {
+    "revision": "6e33be49251dd55fc69c",
+    "url": "/static/js/4.bfa85669.chunk.js"
+  },
+  {
+    "revision": "2b2f1b095f59b1e77f96",
+    "url": "/static/js/5.c96a2983.chunk.js"
+  },
+  {
+    "revision": "891f169d9386360ce797",
+    "url": "/static/js/6.e7a4fadf.chunk.js"
+  },
+  {
+    "revision": "71c3c61bf1a5fc7f4137",
+    "url": "/static/js/main.a3d56d43.chunk.js"
+  },
+  {
+    "revision": "7ff1233c5462773aee87",
+    "url": "/static/js/runtime~main.b78bfed9.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/27.1burger-logo.png.b8503d26.png"
+  }
+]);
